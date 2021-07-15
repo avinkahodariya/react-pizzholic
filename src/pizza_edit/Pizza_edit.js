bedcddf18acf795e73a5c9416a9b93f714bd8fcf
avinkahodariya <avinkahodariya@gmail.com>
2021-07-15 19:08:44 +0530

    pizza edit page functionality and design

--- src/pizza_edit/Pizza_edit.js
+++ src/pizza_edit/Pizza_edit.js
@@ -2,20 +2,24 @@ import React from "react";
 import Pizzanav from "./Pizzanav";
 import Pizza_Edit_Portion from "./Pizza_Edit_Portion";
 import styles from "./pizzaedit.module.css";
-
+import Pizzabox from "../Pizzabox";
+import { useLocation } from "react-router-dom";
+import italian from "../italian.jpg";
 const Pizza_edit = (props) => {
+  const location = useLocation();
+  console.log(location.state.data);
   return (
     <div className={styles.pizzaedit}>
       <div className={styles.pizzanavbar}>
         <Pizzanav
-        // ingredients={e.ingredients}
-        // price={e.price}
-        // name={e.name}
-        // id={e.id}
-        // image={e.image}
+          ingredients={location.state.data.ingredients}
+          price={location.state.data.price}
+          name={location.state.data.name}
+          id={location.state.data.id}
+          image={location.state.data.image}
         />
       </div>
-      <div className={styles.image}>
+      <div className={styles.pizzaimage}>
         <Pizza_Edit_Portion />
       </div>
     </div>
