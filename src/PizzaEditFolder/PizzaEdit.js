c0b92016873cc54e4eb3cfc9d57e86cd09b0ebae
avinkahodariya <avinkahodariya@gmail.com>
2021-07-26 19:42:59 +0530

    pizza data in one file and import it in midle

--- src/PizzaEditFolder/PizzaEdit.js
+++ src/PizzaEditFolder/PizzaEdit.js
@@ -4,7 +4,7 @@ import EditPortion from "./EditPortion";
 import styles from "./pizzaedit.module.css";
 import Pizzabox from "../compo/Pizzabox";
 import { useLocation } from "react-router-dom";
-
+import * as All from "../assets/images";
 import Footer from "../global/Footer";
 // import cartbutton from "./assets/cart-icon.png";
 const PizzaEdit = (props) => {
@@ -25,23 +25,19 @@ const PizzaEdit = (props) => {
           event.target.name,
           ingredients.filter((e) => {
             if (event.target.name == e[0]) {
-              // console.log(event.target.name, e[0], e[1]);
               return e[1];
             }
           }),
         ],
       ]);
     } else {
-      console.log(checkedItems);
       let name = event.target.getAttribute("name");
 
       setCheckedItems(checkedItems.filter((item) => item[0] !== name));
     }
-    console.log(checkedItems);
   };
 
   const AddToCart = () => {
-    console.log(checkedItems);
     console.log(location.state.data.id, location.state.data.price);
     let data = JSON.parse(localStorage.getItem("data"));
     if (data == undefined || data == null) {
@@ -59,22 +55,30 @@ const PizzaEdit = (props) => {
   {
     window.scrollTo(0, 0);
   }
+  console.log(location.state.data.ingredients, "dfg");
   return (
     <div>
       <p className={styles.pizzatitle}>
         Let`s Crete Your Own Pizza
         <img
+          src={All.icon}
           style={{
             width: "50px",
             height: "100%",
             float: "right",
             marginRight: "5px",
+            backdropFilter: "bllur(10px)",
           }}
         />
       </p>
       <div className={styles.pizzarelative}>
         {" "}
-        <EditPortion reciepe={checkedItems} />
+        {/* <EditPortion
+          reciepe={checkedItems}
+          name={location.state.data.name}
+          price={location.state.data.price}
+          ingreinfo={location.state.data.ingredients}
+        /> */}
       </div>
 
       <NavForIngrediants
--- src/compo/Midle.js
+++ src/compo/Midle.js
@@ -3,89 +3,10 @@ import Twit from "./Twit";
 import style from "../pizza.module.css";
 import Pizzabox from "./Pizzabox";
 import * as Al from "../assets/images";
+import PizzaArray from "../global/PizzaArray";
 
 const Midle = () => {
-  const pizza = [
-    {
-      name: "Italian Pizza",
-      ingredients: [
-        ["Tomato", Al.Tomato],
-        ["Brokoli", Al.Brokoli],
-        ["Onion", Al.Onion],
-        ["Olive", Al.Olive],
-        ["Mozzarella", Al.Mozzarella],
-      ],
-      price: "300",
-      id: "1",
-      image: Al.italian,
-    },
-
-    {
-      name: "Pasta Pizza",
-      ingredients: [
-        ["Capsicum", Al.Capsicum],
-        ["Onion", Al.Onion],
-        ["Corn", Al.Corn],
-        ["Tomato", Al.Tomato],
-        ["Mozzarella", Al.Mozzarella],
-      ],
-      price: "400",
-      id: "2",
-      image: Al.pasta,
-    },
-
-    {
-      name: "Mushroom Pizza",
-      ingredients: [
-        ["Capsicum", Al.Capsicum],
-        ["Onion", Al.Onion],
-        ["Mushroom", Al.Mushroom],
-        ["Mozzarella", Al.Mozzarella],
-      ],
-      price: "500",
-      id: "3",
-      image: Al.masroom,
-    },
-
-    {
-      name: "Pataka Paneer",
-      ingredients: [
-        ["Capsicum", Al.Capsicum],
-        ["Olive", Al.Olive],
-        ["Tomato", Al.Tomato],
-        ["Corn", Al.Corn],
-      ],
-      price: "350",
-      id: "4",
-      image: Al.paneer,
-    },
-
-    {
-      name: "Veg Mexican",
-      ingredients: [
-        ["Capsicum", Al.Capsicum],
-        ["Onion", Al.Onion],
-        ["Tomato", Al.Tomato],
-        ["Mozzarella", Al.Mozzarella],
-      ],
-      price: "380",
-      id: "5",
-      image: Al.mexican,
-    },
-
-    {
-      name: "Veggie Delight",
-      ingredients: [
-        ["Capsicum", Al.Capsicum],
-        ["Onion", Al.Onion],
-        ["Tomato", Al.Tomato],
-        ["Mozzarella", Al.Mozzarella],
-      ],
-      price: "280",
-      id: "6",
-      image: Al.veg,
-    },
-  ];
+  const pizza = PizzaArray;
 
   return (
     <div className={style.midleflex}>
